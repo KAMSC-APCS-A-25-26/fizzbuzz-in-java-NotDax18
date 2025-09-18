054f2f3ffaf0f56d2e091b3aec75a2ae269b9bb2
NotDax18 <dax.cw.robinson@gmail.com>
2025-09-18 13:09:06 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -14,12 +14,12 @@ public class FizzBuzz {
             System.out.print(i + " ");
 
             if (i % 15 == 0) {
-                System.out.println("fizzBuzz");
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }
+            } else System.out.println();
         }
     }
 }
